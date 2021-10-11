494671d61d38a09af957791b0f3bb2d26ebdd465
Vikas Mishra <92010949+Vikas66353@users.noreply.github.com>
2021-10-11 21:19:51 +0530

    version 1.0

---
+++ JAVA/Geekster/src/assignment/greatestNUMBER.java
@@ -0,0 +1,23 @@
+package assignment;
+
+public class greatestNUMBER {
+	public static void main(String args[])
+	{
+		int a = 50;
+		int b = 30;
+		int c = 20;
+		if(a>b && a>c)
+		{
+			System.out.print("a is greater than b and c");
+		}
+		else if (b>a && b>c)
+		{
+			System.out.print("b is greater than a and c");
+		}
+		else
+		{
+			System.out.print("c is greater than b and a");
+		}
+	}
+
+}
---
+++ JAVA/Geekster/src/assignment/test1.java
@@ -0,0 +1,9 @@
+package assignment;
+
+public class test1 {
+	public static void main(String args[])
+	{
+		System.out.println("hello world..");
+	}
+
+}
